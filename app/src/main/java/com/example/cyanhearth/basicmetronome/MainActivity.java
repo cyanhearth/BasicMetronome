b934304257a23744f399511bbb7d56c3e752bd58
Craig Whyte <cyanhearth@gmail.com>
2015-01-19 19:54:29 +0000

    Beat emphasis and time signatures
    * First beat of the bar is now played louder than the rest
    * Multiple time signatures are now available
    * changed from Timer to Handler to schedule the repeating task of
    playing the sound

--- app/src/main/java/com/example/cyanhearth/basicmetronome/MainActivity.java
+++ app/src/main/java/com/example/cyanhearth/basicmetronome/MainActivity.java
@@ -5,6 +5,7 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -24,21 +25,46 @@ import java.util.TimerTask;
 
 public class MainActivity extends ActionBarActivity {
 
+    // SoundPool to handle each sound and HashMap to contain references
     private SoundPool pool;
     private HashMap<String, Integer> soundPoolMap;
-    private Button button;
-    private String sound_id;
-    private int tempo;
-    private int period;
 
+    // references to UI elements
     private EditText tempoSelection;
     private TextView notifyError;
-
     private Spinner soundsSpinner;
+    private Spinner timeSigSpinner;
+    private Button button;
 
-    private Timer timer;
-    private TimerTask repeat;
+    // used to find the sound in the hashmap
+    private String sound_id;
 
+    //used to set the appropriate time signature
+    private String timeSig;
+
+    // tempo in bpm
+    private int tempo;
+
+    // period in ms
+    private int period;
+
+    // handler to play the sound at appropriate intervals
+    private Handler handler;
+    private Runnable repeat;
+
+    // to keep track of which beat is being played
+    private int beat;
+
+    // which beat should be emphasised
+    private int emphasis;
+
+    // modify period to fit the time signature
+    private int modifier;
+
+    // the volume at which the sound is to be played
+    private float volume;
+
+    //reference to the resources
     private Resources res;
 
     @Override
@@ -49,7 +75,7 @@ public class MainActivity extends ActionBarActivity {
 
         res = getResources();
 
-        timer = new Timer();
+        handler = new Handler();
 
         button = (Button) findViewById(R.id.button);
 
@@ -71,7 +97,56 @@ public class MainActivity extends ActionBarActivity {
         soundsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // this matches the key in the hashmap
                 sound_id = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // initialize and populate the time signature spinner
+        timeSigSpinner = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> timeSigAdapter = ArrayAdapter.createFromResource(this,
+                R.array.time_sigs, android.R.layout.simple_spinner_item);
+
+        timeSigAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        timeSigSpinner.setAdapter(timeSigAdapter);
+
+        timeSigSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                timeSig = (String) parent.getItemAtPosition(position);
+
+                // set modifier and emphasis values to match the time signature
+                switch (timeSig) {
+                    case "2/4":
+                        modifier = 1;
+                        emphasis = 2;
+                        break;
+                    case "3/4":
+                        modifier = 1;
+                        emphasis = 3;
+                        break;
+                    case "4/4":
+                        modifier = 1;
+                        emphasis = 4;
+                        break;
+                    case "4/8":
+                        modifier = 2;
+                        emphasis = 4;
+                        modifier = 2;
+                        break;
+                    default:
+                        modifier = 1;
+                        emphasis = 4;
+                        break;
+                }
+
             }
 
             @Override
@@ -96,13 +171,18 @@ public class MainActivity extends ActionBarActivity {
             pool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
         }
 
+        // add the sounds to the SoundPool
         soundPoolMap.put("Blip", pool.load(this, R.raw.blip, 1));
         soundPoolMap.put("Boop", pool.load(this, R.raw.boop, 1));
+
+        // initialize the beat
+        beat = 0;
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // reset the error message
                 notifyError.setText("");
 
                 //Get the value the user entered for the tempo
@@ -110,38 +190,64 @@ public class MainActivity extends ActionBarActivity {
 
                 if (tempo < 40 || tempo > 200) {
 
+                    // notify the user if an invalid tempo has been entered
                     notifyError.setText(res.getString(R.string.wrong_tempo));
                     return;
 
                 }
 
+                // make sure the tempo can't be altered once the metronome has been started
                 tempoSelection.setFocusable(false);
 
-                // period in milliseconds
-                period = 60000 / tempo;
-
-                //id = soundPoolMap.get(1);
-
+                // button behaviour if the metronome is not running
                 if (button.getText().toString().equals(res.getString(R.string.button_text_start))) {
 
-                    repeat = new TimerTask() {
+                    repeat = new Runnable () {
+
                         @Override
                         public void run() {
-                            pool.play(soundPoolMap.get(sound_id), 1, 1, 1, 0, 1);
+
+                            if (beat >= emphasis) {
+                                beat = 0;
+                            }
+
+                            if (beat == 0) {
+                                //first beat should be emphasised
+                                volume = 1.0f;
+                            }
+                            else {
+                                volume = 0.3f;
+                            }
+
+                            // period in milliseconds
+                            period = (60000 / tempo) / modifier;
+
+                            // play the sound from the SoundPool
+                            pool.play(soundPoolMap.get(sound_id), volume, volume, 1, 0, 1);
+                            beat++;
+
+                            handler.postDelayed(repeat, period);
+
                         }
                     };
 
-                    timer.schedule(repeat, 0, period);
+                    // start the runnable
+                    handler.postDelayed(repeat, period);
 
                     button.setText(res.getString(R.string.button_text_stop));
 
                 }
                 else {
 
-                    repeat.cancel();
+                    // stop the runnable
+                    handler.removeCallbacks(repeat);
+
+                    //reset beat
+                    beat = 0;
 
                     button.setText(res.getString(R.string.button_text_start));
 
+                    // allow tempo to be changed again
                     tempoSelection.setFocusableInTouchMode(true);
 
                 }
